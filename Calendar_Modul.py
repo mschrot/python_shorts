706e11d2846fe3abf411ce5809cfdd8555bf54d3
mschrot <mschrot@protonmail.com>
2025-06-02 16:21:45 +0200

    Kalender mit Python programmieren.

---
+++ Calendar_Modul.py
@@ -0,0 +1,10 @@
+import calendar  # import --> module (os, math, datatime)
+
+
+myapp = calendar.month(2025, 6, 1)
+# var --> modul.fun(arg)
+# modull --> klassen, konst., funkt.
+
+
+print(myapp)
+# print --> ausgabefukt --> app
