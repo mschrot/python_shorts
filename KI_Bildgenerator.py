13c64e9d6ea5e853ba96804ec2998d6b2346e3c3
mschrot <mschrot@protonmail.com>
2025-06-03 20:44:14 +0200

    KI - Bildgenerator mit Python programmieren

--- KI_Bildgenerator.py
+++ KI_Bildgenerator.py
@@ -1,20 +1,12 @@
 # https://youtube.com/shorts/hinplDA-kUw?si=1jC_Zmsh76goGYTE
 
 from g4f.client import Client
-
 text_eingabe = input('Eingabe:')
 app_client = Client()
-
-
 my_model = app_client.images.generate(
     model='flux',
     prompt=text_eingabe,
     response_format='url'
 )
-
-
-
 image_url = my_model.data[0].url
-
-
 print(f'Link zum Bild: \n{image_url}')
