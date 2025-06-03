0461ecfeb2ea6e262834db03baf11a628d3fc0f4
mschrot <mschrot@protonmail.com>
2025-06-03 21:25:50 +0200

    QR-Codes mit Python programmieren

--- QR_Codes.py
+++ QR_Codes.py
@@ -1,6 +1,8 @@
 # https://youtube.com/shorts/KnmwX_vqyvI?si=W41xpTyjD7b8haMN
 
-import qrcode
-qr_inhalt = 't.me/mschrot'
-img = qrcode.make(qr_inhalt)
-img.save('qrcode.png')
+import qrcode  # Import qrcode modull
+
+
+qr_inhalt = 't.me/mschrot'    # Variable inhalt wird in QR-Code gespeichert
+img = qrcode.make(qr_inhalt)  # Erstellt das QR-Code-Bild-Objekt   
+img.save('qrcode.png')        # Speichert die Bilddatei mit dem Namen 'qrcode.png'
