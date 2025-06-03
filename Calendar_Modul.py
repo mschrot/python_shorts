754e9311f4f074595efb9826801d88b55f246810
mschrot <mschrot@protonmail.com>
2025-06-03 21:32:05 +0200

    Kalender mit Python programmieren

--- Calendar_Modul.py
+++ Calendar_Modul.py
@@ -1,5 +1,6 @@
 # https://youtube.com/shorts/KnmwX_vqyvI?si=a92-UrCDQgBXC8TH
 
-import calendar
-myapp = calendar.month(2025, 6, 1)
-print(myapp)
+import calendar  # Importiert das calendar-Modul, um mit Datumsfunktionen zu arbeiten
+
+myapp = calendar.month(2025, 6)    # Jahr und Monat als Kalendar ausgeben (Y, M)
+print(myapp)  # # Gibt den Monatskalender in der Konsole aus
