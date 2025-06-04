72b2578a009d05e30aba2e03925187c3f101c8fc
mschrot <mschrot@protonmail.com>
2025-06-04 12:17:22 +0200

    Emoji Zufallsgenerator

---
+++ Emoji_Zufallsgenerator.py
@@ -0,0 +1,10 @@
+
+
+# Unicode Emoji 🔥 💡 🧪 🎲 
+import random   # import Modull
+
+
+# Zufallszahlen zu erzeugen
+print("🎲", random.randint(1, 6))   # print("unicode", modul_random.funktion_randint(arg_1, arg_2)
+                                    # Unicode: "\U0001F3B2" --> 🎲
+                                    
